51cf40fddafa0185429d4307d01ea98b6e752857
Chiril Babaev <babaechi@fel.cvut.cz>
2022-06-21 21:49:48 +0200

    cluster hazelcast

--- Copyto/User/src/main/java/fel/cvut/user/UserApplication.java
+++ Copyto/User/src/main/java/fel/cvut/user/UserApplication.java
@@ -3,11 +3,13 @@ package fel.cvut.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCaching
+@EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
